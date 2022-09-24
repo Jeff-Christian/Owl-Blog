d33e9e18b49b026db7eac62bc5c37759b1cfed6b
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-09-24 19:28:23 -0300

    styling main page of login, added new icon from footer, added too fontawesome for icons in page and links for fonts

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,37 @@
 import styles from './Login.module.css';
 
+// Fontawesome, Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+
+// Images
+import logoSocial from "../../images/logo-social.png";
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <nav>
+        <ul>
+          <li><img src={logoSocial} alt="logo social" className={styles.logo} /></li>
+        </ul>
+      </nav>
+      <div className={styles.register}>
+        <div className={styles.newRegister}>
+          <FontAwesomeIcon icon={faUserPlus} />
+          <div className={styles.frameBottom}>
+          <button>cadastrar</button>
+          </div>
+        </div>
+        <div className={styles.registerForm}>
+          <form>
+            <input type="email" name="email" id="email" placeholder='Email' required/>
+            <input type="password" name="password" id="password" placeholder='Senha' required/>
+            <input type="submit" value="Entrar" />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
