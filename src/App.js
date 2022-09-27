a480a719b669d2f1e0d4fdb2d217bfaf25bce1aa
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-09-27 16:16:24 -0300

    provider of authentication informing all pages user authenticated. also bar loading animated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Context
 import { AuthProvider } from './context/AuthContext';
 
+// Firebase
+import { onAuthStateChanged } from 'firebase/auth';
+
 // Pages
 import Login from './Pages/Login/Login';
 import Home from './Pages/Home/Home';
@@ -14,12 +17,36 @@ import Home from './Pages/Home/Home';
 import Footer from "./Components/Footer/Footer";
 import NotFound from "./Pages/Not Found/NotFound";
 
+// hooks
+import {useState, useEffect} from "react";
+import {useAuthentication} from './hooks/useAuthentication';
+
 
 function App() {
+
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication();
+
+  // attributed loading
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+
+  }, [auth]);
+
+  // change at some point to a screen loading bar
+  if(loadingUser){
+    return <div className='loader'></div>
+  }
+
   return (
     <>
     <div className='container'>
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Login></Login>}></Route>
