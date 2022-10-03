6fb65fb4f2c385c69fca1ad3785b1f29a40ed7d8
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-10-03 17:20:00 -0300

    styling component of individual post

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,7 +1,16 @@
 import styles from './Post.module.css';
 
+// images
+import icon from "../../images/icon-esc.png";
+
+// Icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+
 // Route
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Hooks
 import { useFetchDocument } from '../../hooks/useFetchDocument';
@@ -13,12 +22,41 @@ const Post = () => {
 
 
   return (
-    <div className={styles.post}>
-      Post: {id}
+    <div className='container'>
       {post && (
         <>
-          Post: {post.id}
-          <h1>{post.body}</h1>
+        <div className={styles.postContainer}>
+
+          <div className={styles.frameImage}>
+
+            <div className={styles.escField}>
+              <Link to="/home"><FontAwesomeIcon icon={faXmark}/></Link>
+              <img src={icon} alt="logo social media" />
+            </div>
+
+            <div className={styles.image}>
+              <img src={post.image} alt={post.body} />
+            </div>
+          </div>
+
+          <div className={styles.frameComments}>
+
+            <div className={styles.profile}>
+
+              <div className={styles.user}>
+                <span><FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon></span>
+                <p>{post.createdBy}</p>
+              </div>
+
+              <div className={styles.desc}>
+                <p>{post.body}</p>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
         </>
       )}
     </div>
